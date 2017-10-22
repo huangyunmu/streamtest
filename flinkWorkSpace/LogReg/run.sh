9cac5793624e03f9046db1e7dcac5e95a324ff92
huangyunmu <andyhuangyunmu@gmail.com>
2017-10-22 21:12:47 +0800

    Test

--- flinkWorkSpace/LogReg/run.sh
+++ flinkWorkSpace/LogReg/run.sh
@@ -1,5 +1,5 @@
 /data/opt/course/1155086998/flink/bin/flink run \
--c Test \
+-c Flink_Test.LogReg.Test \
 ./target/LogReg-0.0.1.jar \
 --input file:///data/opt/course/1155086998/data/logistic.txt \
 2>&1 | tee -a log.txt
