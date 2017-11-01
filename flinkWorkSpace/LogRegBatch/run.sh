2a7713c1391e6814802415b33daffc02fe259aef
huangyunmu <andyhuangyunmu@gmail.com>
2017-11-01 14:20:06 +0800

    Test 1420

--- flinkWorkSpace/LogRegBatch/run.sh
+++ flinkWorkSpace/LogRegBatch/run.sh
@@ -1,6 +1,5 @@
 /data/opt/course/1155086998/flink/bin/flink run \
 -c Flink_Test.LogRegBatch.LinearRegression \
-./target/LogReg-0.0.1.jar \
---input file:///data/opt/course/1155086998/data/logistic.txt \
+./target/LogRegBatch-0.0.1.jar \
 --output file:///data/opt/course/1155086998/data/output-batch.txt \
 2>&1 | tee -a log.txt
