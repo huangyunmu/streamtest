4757a17f4ff051b4ae3b33c6ed3d79604a1a9d4a
huangyunmu <andyhuangyunmu@gmail.com>
2017-11-02 16:21:12 +0800

    Test 1621

--- flinkWorkSpace/LogRegSingle/run.sh
+++ flinkWorkSpace/LogRegSingle/run.sh
@@ -2,5 +2,5 @@
 -c Flink_Test.LogReg.Test \
 ./target/LogReg-0.0.1.jar \
 --input file:///data/opt/course/1155086998/data/logistic.txt \
---output file:///data/opt/course/1155086998/data/output.txt \
+--output file:///data/opt/course/1155086998/data/output-single.txt \
 2>&1 | tee -a log.txt
