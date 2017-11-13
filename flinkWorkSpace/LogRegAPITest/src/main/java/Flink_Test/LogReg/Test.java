2b3244f8053b3f848374243cb14b49a38a0746eb
huangyunmu <andyhuangyunmu@gmail.com>
2017-11-13 17:05:53 +0800

    Test 1705

--- flinkWorkSpace/LogRegAPITest/src/main/java/Flink_Test/LogReg/Test.java
+++ flinkWorkSpace/LogRegAPITest/src/main/java/Flink_Test/LogReg/Test.java
@@ -67,12 +67,12 @@ public class Test {
 			public Tuple2<Integer, Float[]> map(String value) throws Exception {
 				// TODO Auto-generated method stub
 				Tuple2<Integer, Float[]> tempTuple = new Tuple2<Integer, Float[]>();
-				tempTuple.f0 = 1;
+				tempTuple.f0 = value.length();
 				tempTuple.f1 = new Float[] { 2f, 3f };
 //				String[] split = value.split("\t");
 //				int dim = split.length - 1;
 				// // Label
-				tempTuple.f0 = Integer.parseInt(value.substring(value.length()-1));
+//				tempTuple.f0 = Integer.parseInt(value.substring(value.length()-1));
 				// // Feature
 				// Float[] tempFeature = new Float[dim];
 				// for (int i = 0; i < dim; i++) {
