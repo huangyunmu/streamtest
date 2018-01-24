9c868b02351f39c18b48c0aa235b8440849bf51a
huangyunmu <andyhuangyunmu@gmail.com>
2018-01-24 17:05:24 +0800

    Test 1705

--- script/start_zoolkeeperAndkafka.py
+++ script/start_zoolkeeperAndkafka.py
@@ -1,10 +1,10 @@
 import os
 kafkaPath="/data/opt/course/1155086998/kafka/"
-cmd ="."+kafkaPath+"bin/zookeeper-server-start.sh"+ kafkaPath+" config/zookeeper.properties"
+cmd ="."+kafkaPath+"bin/zookeeper-server-start.sh "+ kafkaPath+"config/zookeeper.properties"
 print(cmd)
 #os.system(cmd)
 for i in range(5,11):
     pass
-    #cmd ="."+kafkaPath+"bin/zookeeper-server-start.sh"+ #kafkaPath+"config/zookeeper.properties"
+    #cmd =kafkaPath+"bin/zookeeper-server-start.sh"+ #kafkaPath+"config/zookeeper.properties"
     #os.system(cmd)
 # print cmd
