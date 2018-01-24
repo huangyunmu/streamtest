8278b86b44d9793ca57a60e561dd3c3348177994
huangyunmu <andyhuangyunmu@gmail.com>
2018-01-24 18:58:36 +0800

    Test 1858

--- script/start_kafka.py
+++ script/start_kafka.py
@@ -1,7 +1,7 @@
 import os
 kafkaPath="/data/opt/course/1155086998/kafka/"
-for i in range(5,7):
+for i in range(5,11):
     cmd =kafkaPath+"bin/kafka-server-start.sh "+ kafkaPath+"config/server_proj"+str(i)+".properties"
-    print(cmd)
-    #os.system(cmd)
+    #print(cmd)
+    os.system(cmd)
 # print cmd
