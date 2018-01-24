908171727e7b3d6d6fc1b18325f0aa55a96308ed
huangyunmu <andyhuangyunmu@gmail.com>
2018-01-24 20:20:24 +0800

    Test 2020

--- script/stop_kafka.py
+++ script/stop_kafka.py
@@ -1,5 +1,9 @@
 import os
 kafkaPath="/data/opt/course/1155086998/kafka/"
-cmd =kafkaPath+"bin/zookeeper-server-start.sh "+ kafkaPath+"config/zookeeper.properties"
-os.system(cmd)
+for i in range(5,11):
+    cmd ="ssh 1155086998@proj" + str(i) + " "
+    cmd =cmd+kafkaPath+"bin/kafka-server-stop.sh &"
+    #print(cmd)
+    os.system(cmd)
+    cmd =""
 # print cmd
