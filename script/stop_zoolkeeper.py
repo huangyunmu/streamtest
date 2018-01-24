fdb976562d9582f51eb7d45f5701bcd845d36421
huangyunmu <andyhuangyunmu@gmail.com>
2018-01-24 17:21:06 +0800

    Test 1721

--- script/stop_zoolkeeper.py
+++ script/stop_zoolkeeper.py
@@ -1,5 +1,5 @@
 import os
 kafkaPath="/data/opt/course/1155086998/kafka/"
-cmd =kafkaPath+"bin/zookeeper-server-start.sh "+ kafkaPath+"config/zookeeper.properties"
+cmd =kafkaPath+"bin/zookeeper-server-stop.sh "+ kafkaPath+"config/zookeeper.properties"
 os.system(cmd)
 # print cmd
