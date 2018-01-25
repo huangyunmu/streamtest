a700d11f1a211ff8866079dfb7b11773c00d2dfd
huangyunmu <andyhuangyunmu@gmail.com>
2018-01-25 16:25:59 +0800

    Test 1625

--- python/dataProducer.py
+++ python/dataProducer.py
@@ -2,9 +2,13 @@ import os
 kafkaPath="/data/opt/course/1155086998/kafka/"
 fileName="a9"
 topic="test"
+cmd=kafkaPath+"bin/kafka-console-producer.sh --broker-list localhost:9092 --topic "+topic+" "+data
+#os.system(cmd)
 while True:
     with open(fileName,"r") as f:
-        data=f.read()
-		cmd=kafkaPath+"bin/kafka-console-producer.sh --broker-list localhost:9092 --topic "+topic+data
-		os.system(cmd)
-		#print()
+        data=f.readline()
+        print(data)
+        #cmd=kafkaPath+"bin/kafka-console-producer.sh --broker-list localhost:9092 --topic "+topic+" "+data
+        #os.system(cmd)
+    #print(cmd)   
+    break
