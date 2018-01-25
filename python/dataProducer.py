d01796c8dce404b89a6b2fb8955d82c91431c4da
huangyunmu <andyhuangyunmu@gmail.com>
2018-01-25 16:09:27 +0800

    Test 1609

--- python/dataProducer.py
+++ python/dataProducer.py
@@ -1,4 +1,10 @@
+import os
+kafkaPath="/data/opt/course/1155086998/kafka/"
 fileName="a9"
+topic="test"
 while True:
     with open(fileName,"r") as f:
-        print(f.read())
+        data=f.read()
+		cmd=kafkaPath+"bin/kafka-console-producer.sh --broker-list localhost:9092 --topic "+topic+data
+		os.system(cmd)
+		#print()
