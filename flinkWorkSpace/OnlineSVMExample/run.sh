b5ab24d4e0125342b827ea9ebfab97f9f8d33841
huangyunmu <andyhuangyunmu@gmail.com>
2018-01-26 14:02:12 +0800

    Test 1402

--- flinkWorkSpace/OnlineSVMExample/run.sh
+++ flinkWorkSpace/OnlineSVMExample/run.sh
@@ -1,9 +1,9 @@
 /data/opt/course/1155086998/flink/bin/flink run \
 -c Flink_Test.OnlineSVMExample.OnlineSVMExample \
 ./target/OnlineSVMExample-0.0.1.jar \
---data.topic a9_data_topic
---feature.num 123
---learning.rate 0.01
---update.frequency 1
---grad.topic a9_online_svm_grad
+--data.topic a9_data_topic \
+--feature.num 123 \
+--learning.rate 0.01 \
+--update.frequency 1 \
+--grad.topic a9_online_svm_grad \
 2>&1 | tee -a log.txt
