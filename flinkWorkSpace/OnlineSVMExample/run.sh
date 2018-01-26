9e8204d172e39dd02565c90a28aefd4d02041174
huangyunmu <andyhuangyunmu@gmail.com>
2018-01-26 15:36:13 +0800

    Test 1536

--- flinkWorkSpace/OnlineSVMExample/run.sh
+++ flinkWorkSpace/OnlineSVMExample/run.sh
@@ -1,6 +1,7 @@
 /data/opt/course/1155086998/flink/bin/flink run \
 -C file:///data/opt/course/1155086998/lib/flink-connector-kafka-0.10_2.11-1.3.2.jar \
 -C file:///data/opt/course/1155086998/lib/flink-connector-kafka-0.9_2.11-1.3.2.jar \
+-C file:///data/opt/course/1155086998/lib/flink-connector-kafka-base_2.11-1.3.2.jar
 -c Flink_Test.OnlineSVMExample.OnlineSVMExample \
 ./target/OnlineSVMExample-0.0.1.jar \
 --data.topic a9_data_topic \
