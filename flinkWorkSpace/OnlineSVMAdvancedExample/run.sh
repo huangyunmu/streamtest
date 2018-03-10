5f4c500d31f64962d8f5897147cf0e1a9e4acb66
huangyunmu <andyhuangyunmu@gmail.com>
2018-03-10 16:42:13 +0800

    Test 1642

--- flinkWorkSpace/OnlineSVMAdvancedExample/run.sh
+++ flinkWorkSpace/OnlineSVMAdvancedExample/run.sh
@@ -9,7 +9,7 @@
 -c Flink_Test.OnlineSVMExample.OnlineSVMExample \
 ./target/OnlineSVMExample-0.0.1.jar \
 --train.frequency 5000 \
---data.topic a9_test_12_data_topic \
+--data.topic a9_test_13_data_topic \
 --temp.topic a9_temp_5_topic \
 --feature.num 124 \
 --learning.rate 0.01 \
