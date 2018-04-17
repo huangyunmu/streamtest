2c510409907930133e17a2bfd25649c9708754fe
huangyunmu <andyhuangyunmu@gmail.com>
2018-04-17 22:45:39 +0800

    Test 2245

--- flinkWorkSpace/OnlineSVMIterExample/src/main/java/Flink_Test/OnlineSVMExample/OnlineSVMExample.java
+++ flinkWorkSpace/OnlineSVMIterExample/src/main/java/Flink_Test/OnlineSVMExample/OnlineSVMExample.java
@@ -4,9 +4,11 @@ import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class OnlineSVMExample {
+	final static int PARALLELISM=40;
     static public void main(String[] args) throws Exception {
         OnlineSVMModel svm = new OnlineSVMModel(args);
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(PARALLELISM);
         env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.AT_LEAST_ONCE);
         svm.modeling(env);
         env.execute("online svm alpha");
