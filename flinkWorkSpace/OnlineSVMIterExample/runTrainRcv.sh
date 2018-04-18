e9b033427438ad0a592df644d5d8540cb78bebc9
huangyunmu <andyhuangyunmu@gmail.com>
2018-04-18 19:56:59 +0800

    Test 1956

--- flinkWorkSpace/OnlineSVMIterExample/runTrainRcv.sh
+++ flinkWorkSpace/OnlineSVMIterExample/runTrainRcv.sh
@@ -8,12 +8,12 @@
 -C file:///data/opt/course/1155086998/lib/flink-ml_2.11-1.3.2.jar \
 -c Flink_Test.OnlineSVMExample.OnlineSVMExample \
 ./target/OnlineSVMExample-0.0.1.jar \
---train.frequency 2500 \
+--train.frequency 5000 \
 --data.topic rcv_train_4_part_data_topic \
 --feature.num 47237  \
 --learning.rate 0.01 \
 --update.frequency 250 \
---grad.topic rcv_online_svm_7_grad \
+--grad.topic rcv_online_svm_8_grad \
 --bootstrap.servers proj10:9092,proj9:9092,proj8:9092,proj7:9092,proj6:9092,proj5:9092 \
 --zookeeper.connect localhost:2181 \
 2>&1 | tee -a log.txt
