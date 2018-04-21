4677b6bd2d33f36acb6d5b559924fdf91d9fcf5a
huangyunmu <andyhuangyunmu@gmail.com>
2018-04-21 15:38:16 +0800

    Test 1538

--- flinkWorkSpace/OnlineSVMAlphaExample/runTestRcv.sh
+++ flinkWorkSpace/OnlineSVMAlphaExample/runTestRcv.sh
@@ -9,11 +9,11 @@
 -c Flink_Test.OnlineSVMExample.OnlineSVMExample \
 ./target/OnlineSVMExample-0.0.1.jar \
 --train.frequency 20 \
---data.topic rcv_test_2_data_topic \
+--data.topic rcv_test_3_data_topic \
 --feature.num 47237  \
 --learning.rate 0.01 \
 --update.frequency 250 \
---grad.topic rcv_online_svm_27_grad \
+--grad.topic rcv_online_svm_28_grad \
 --bootstrap.servers proj10:9092,proj9:9092,proj8:9092,proj7:9092,proj6:9092,proj5:9092 \
 --zookeeper.connect localhost:2181 \
 --rawdata.parallelism 10 \
@@ -22,5 +22,5 @@
 --iteration.waitTime 4000 \
 --newdata.countfrequency 50 \
 --alldata.countfrequency 1000 \
---param.outputfrequency 10000 \
+--param.outputfrequency 20000 \
 2>&1 | tee -a log.txt
