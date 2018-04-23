61d1dc2a3a8b321eb8d918c60b1f14d891fd6600
huangyunmu <andyhuangyunmu@gmail.com>
2018-04-23 21:14:45 +0800

    Test 2114

--- flinkWorkSpace/OnlineSVMAlphaExample/runTrainA9.sh
+++ flinkWorkSpace/OnlineSVMAlphaExample/runTrainA9.sh
@@ -8,11 +8,11 @@
 -C file:///data/opt/course/1155086998/lib/flink-ml_2.11-1.3.2.jar \
 -c Flink_Test.OnlineSVMExample.OnlineSVMExample \
 ./target/OnlineSVMExample-0.0.1.jar \
---train.frequency 5 \
+--train.frequency 1 \
 --data.topic a9_train_2_topic \
 --feature.num 124  \
 --learning.rate 0.01 \
---update.frequency 250 \
+--update.frequency 50 \
 --grad.topic a9_online_svm_1_grad \
 --bootstrap.servers proj10:9092,proj9:9092,proj8:9092,proj7:9092,proj6:9092,proj5:9092 \
 --zookeeper.connect localhost:2181 \
